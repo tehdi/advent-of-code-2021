550f14d6c0be5cd53f3c9db882ed3544193846b3
tehdi <tehdi@users.noreply.github.com>
2021-12-02 11:56:59 -0400

    Day 2 Part 2: up and down don't do what I thought they did

---
+++ day02/part2.go
@@ -0,0 +1,42 @@
+package main
+ 
+import (
+    "fmt"
+    "os"
+    "bufio"
+    "strconv"
+    "strings"
+)
+ 
+func main() {
+    file, fileError := os.Open("input")
+    if fileError != nil {
+        fmt.Println(fileError)
+    }
+    defer file.Close()
+
+    var aim, horizontal, depth int
+    scanner := bufio.NewScanner(file)
+    for scanner.Scan() {
+        line := scanner.Text()
+        parts := strings.Split(line, " ")
+
+        direction := parts[0]
+        value, _ := strconv.Atoi(parts[1])
+
+        switch direction {
+            case "forward":
+                horizontal += value
+                depth += value * aim
+            case "down":
+                aim += value
+            case "up":
+                aim -= value
+        }
+        fmt.Printf("%s %d: a=%d => h:%d * d:%d = %d\n", direction, value, aim, horizontal, depth, horizontal * depth)
+    }
+        
+    if scannerError := scanner.Err(); scannerError != nil {
+        fmt.Println(scannerError)
+    }
+}
