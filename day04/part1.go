57c4428c80be17f2fb36206da10d901502024d49
tehdi <tehdi@users.noreply.github.com>
2021-12-05 13:50:34 -0400

    Day 4 Part 1: a lot of loops and janky control flow to find the score for the winning board

--- day04/part1.go
+++ day04/part1.go
@@ -5,6 +5,8 @@ import (
     "fmt"
     "os"
     "bufio"
+    "strings"
+    "strconv"
 )
 
 type boardPosition struct {
@@ -14,14 +16,55 @@ type boardPosition struct {
 }
 
 type bingoBoard struct {
+    rows []string
     numbers map[int]boardPosition
     rowCounts, columnCounts map[int]int
     diagonalCounts map[string]int
 }
 
+func NewBoard() bingoBoard {
+    board := bingoBoard {
+        rows: make([]string, 0),
+        numbers: make(map[int]boardPosition),
+        rowCounts: make(map[int]int),
+        columnCounts: make(map[int]int),
+        diagonalCounts: make(map[string]int),
+    }
+    return board
+}
+
+func (b *bingoBoard) AddRow(row string) bool {
+    b.rows = append(b.rows, row)
+
+    //fmt.Printf("Appended row to board: %s; board now contains: %s\n", row, b.rows)
+    return len(b.rows) == 5 // 5 rows = board is done
+}
+
+func (b *bingoBoard) ParseRows() {
+    for rowIndex, row := range b.rows {
+        // Fields "splits the string s around each instance of one or more consecutive white space characters"
+        columns := strings.Fields(row)
+        for columnIndex, column := range columns {
+            diagonal := CalculateDiagonal(rowIndex, columnIndex)
+            number, _ := strconv.Atoi(column)
+            b.numbers[number] = boardPosition {
+                row: rowIndex,
+                column: columnIndex,
+                diagonal: diagonal,
+            }
+        }
+    }
+
+    //fmt.Printf("Parsed rows to board. Board now contains: %s\n", b.numbers)
+}
+
 func (b *bingoBoard) MarkNumber(number int) bool {
     // "comma ok" idiom for "if key is in map"
     if boardPosition, ok := b.numbers[number]; ok {
+        boardPosition.marked = true
+        b.numbers[number] = boardPosition // https://stackoverflow.com/a/42716918
+        //fmt.Printf("Found number %d at %+v\n", number, boardPosition)
+
         b.rowCounts[boardPosition.row]++
         b.columnCounts[boardPosition.column]++
         if b.rowCounts[boardPosition.row] == 5 || b.columnCounts[boardPosition.column] == 5 {
@@ -29,13 +72,28 @@ func (b *bingoBoard) MarkNumber(number int) bool {
         }
 
         if boardPosition.diagonal != "none" {
-            b.diagonalCounts[boardPosition.diagonal]++
-            if b.diagonalCounts[boardPosition.diagonal] == 5 {
+            if boardPosition.diagonal == "both" {
+                b.diagonalCounts["up"]++
+                b.diagonalCounts["down"]++
+            } else {
+                b.diagonalCounts[boardPosition.diagonal]++
+            }
+            if b.diagonalCounts["up"] == 5 || b.diagonalCounts["down"] == 5 {
                 return true // winner!
             }
         }
     }
     return false
+}
+
+func (b *bingoBoard) SumUnmarkedNumbers() int {
+    sum := 0
+    for number, boardPosition := range(b.numbers) {
+        if !boardPosition.marked {
+            sum += number
+        }
+    }
+    return sum
 }
 
 func main() {
@@ -58,17 +116,54 @@ func main() {
         fmt.Println(scannerError)
     }
 
-    fmt.Println(values)
-    // numbers = values[0]
-    // then a blank line followed by a board (5 lines),
-    // then more boards that are always 5 lines, and always with blank lines between them
+    //fmt.Println(values)
+    // boards are always 5 lines, and there's always a blank line between them
+    boards := make([]bingoBoard, 0)
+    for _, line := range values[1:] {
+        if len(line) == 0 {
+            boards = append(boards, NewBoard())
+        } else {
+            boardBuilt := boards[len(boards) - 1].AddRow(line)
+            if boardBuilt {
+                //fmt.Printf("Board built: %+v\n", boards[len(boards) - 1])
+                boards[len(boards) - 1].ParseRows()
+            }
+        }
+    }
+    //fmt.Printf("Built %d boards\n", len(boards))
+
+    // numbers to be called
+    numbers := make([]int, 0)
+    for _, value := range strings.Split(values[0], ",") {
+        number, _ := strconv.Atoi(value)
+        numbers = append(numbers, number)
+    }
+    //fmt.Println(numbers)
+
+    for _, number := range numbers {
+        win := false
+        for _, board := range boards {
+            win = board.MarkNumber(number)
+            if win {
+                unmarkedSum := board.SumUnmarkedNumbers()
+                fmt.Printf("Winner on number %d with unmarked sum %d => score %d!\n", number, unmarkedSum, number * unmarkedSum)
+                break
+            }
+        }
+        if win {
+            break
+        }
+    }
 }
 
 func CalculateDiagonal(row, column int) string {
+    // 4 assumes 0-index. change to 6 if you 1-index
+    if row == column && row + column == 4 {
+        return "both"
+    }
     if row == column {
         return "down"
     }
-    // 4 assumes 0-index. change to 6 if you 1-index
     if (row + column == 4) {
         return "up"
     }
