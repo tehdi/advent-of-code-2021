972179c7b95a9a02a4f4eea1d0e966c2b3899492
tehdi <tehdi@users.noreply.github.com>
2023-01-08 22:46:33 -0400

    misc

--- test.sh
+++ test.sh
@@ -4,10 +4,10 @@
 # sh ./run.sh {day} {part}
 # sh ./run.sh 01 1
 
-echo go run ./day$1/part$2.go -input-file=./day$1/test-input
+echo go test ./day$1/part2_test.go -input-file=./day$1/test-input
 
 docker run --rm \
     -v "$PWD":/usr/src/myapp \
     -w /usr/src/myapp \
     golang:1.17 \
-    go run ./day$1/part$2.go -input-file=./day$1/test-input
+    go test ./day$1/part2_test.go
